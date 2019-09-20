1367b19ba8c3b45d9e05816a5b9447a6fda9f43d
KevinSadler <KevinSadler@github.comgit config --global user.email KevinSadler@github.comgit config --global user.email KevinSadler@github.com>
2019-09-20 15:22:35 -0500

    basic charts setup

---
+++ src/components/charts/Charts.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Charts extends Component {
+    render() {
+        return (
+        <section>
+        <img src={require('./stickweldingchart2.jpg')} />
+        <img src={require('./tig-stainless.jpg')} />
+        <img src={require('./weld-symbols.jpg')} />
+        </section>
+        )
+    }
+}
+
+export default Charts
